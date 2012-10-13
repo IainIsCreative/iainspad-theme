fca24d15fb559bd0955d72a039ae151793ee6522
Iain MacDonald <iain@iainspad.com>
2012-10-13 02:31:35 +0100

    Added Pingback

--- header.php
+++ header.php
@@ -20,6 +20,7 @@
 <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php bloginfo('template_url'); ?>/img/apple-touch-icon-ipad.png" />
 <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php bloginfo('template_url'); ?>/img/apple-touch-icon-iphone-retina.png" />
 <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php bloginfo('template_url'); ?>/img/apple-touch-icon-ipad-retina.png" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 <link rel="alternate" type="application/rss+xml" title="iainspad RSS feed" href="<?php bloginfo('rss2_url'); ?>" />
 <script src="//use.typekit.net/sab7qbq.js"></script>
 <script>try{Typekit.load();}catch(e){}</script>
