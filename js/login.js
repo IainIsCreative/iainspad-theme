d7f4ed1244b1bc4de56dfe33e973de5bd385cd25
Iain MacDonald <iain@iainspad.com>
2012-10-14 23:54:29 +0100

    Added login.js

---
+++ js/login.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('#loginform input[type="text"]').attr('placeholder','Username');
+	$('#loginform input[type="password"]').attr('placeholder','Password');
+
+	$('#loginform label[for="user_login"]').contents().filter(function() {
+		return this.nodeType === 3;
+	}).remove();
+	$('#loginform label[for="user_pass"]').contents().filter(function() {
+		return this.nodeType === 3;
+	}).remove();
+});
