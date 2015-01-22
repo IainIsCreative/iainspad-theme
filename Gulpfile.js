1f188092278067f54316ec7b3b6d823498ce8fdf
iain <iain@jp74.com>
2015-01-22 09:43:15 +0000

    Added .gitignore, .editorconfig, Gulpfile, and package.json files. Deleted oldscripts.js file.

---
+++ Gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+	$ = require('gulp-load-plugins')();
+
+gulp.task('scss', function() {
+	gulp.src([
+		'./styles/**/*.scss',
+		'./styles/*.scss'
+	])
+	.pipe($.sass())
+	.pipe($.autoprefixer())
+	.pipe($.minifyCss())
+	.pipe(gulp.dest('./'))
+});
+
+gulp.task('watch', function() {
+	gulp.watch([
+		'./styles/**/*.scss',
+		'./styles/*.scss'
+	], ['sass'])
+});
+
+gulp.task('default', [sass], function() {
+
+});
